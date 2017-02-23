9a63735770e3315f336ed6fa0f8a502f5804a639
Christoph Oelmueller <christoph@oelmueller.info>
2017-02-23 13:42:43 +0100

    add parameter resultkey, var renaming, unknown result on errors

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/zwopir/gonag"
-	_ "github.com/zwopir/check_osquery/model"
 	"github.com/zwopir/check_osquery/osquery"
 	"github.com/prometheus/common/log"
 	"text/template"
@@ -10,23 +9,23 @@ import (
 	"flag"
 	"bytes"
 	"strconv"
-
 	"fmt"
 	"os"
 )
 
 
-
 func main() {
 	const (
-		defaultTemplateString = `osquery returned count = {{ index . "count" }}`
+		defaultTemplateString = `osquery returned result = {{ index . "result(*)" }}`
 	)
 	var (
 		timeoutString = flag.String("timeout", "5s", "osquery timeout. Value must be time.ParseDuration'able")
-		query = flag.String("query", "", "osquery string. Must return a single result set with at least one key/value named 'count'. " +
-			"All other key/value pairs can be used in the output text template as {{ index . \"key\" }}")
+		query = flag.String("query", "", "osquery string. Must return a single osqueryResult. The value of the config parameter 'resultkey' is used " +
+			"in the threshold evaluation (default see there). All other key/value pairs can be used in the output text template as {{ index . \"key\" }}")
+		resultKey = flag.String("resultkey", "count(*)", "osqueryResult key. Specifies the key within the osquery osqueryResult set that holds the " +
+			"value for threshold comparision.")
 		osqueryExecutable = flag.String("osquery","osqueryi", "osquery executable")
-		templateString = flag.String("text template", defaultTemplateString, "nagios result text template")
+		templateString = flag.String("template", defaultTemplateString, "nagios osqueryResult text template")
 		warn = flag.String("warn", "", "warning threshold. Must be empty or parseable as float64")
 		crit = flag.String("crit", "", "critical threshold. Must be empty or parseable as float64")
 	)
@@ -34,44 +33,45 @@ func main() {
 
 	template, err := template.New("nagios output").Parse(*templateString)
 	if err != nil {
-		log.Fatalf("can't parse template %q", *templateString)
+		exitUnknown("can't parse template %q", *templateString)
 	}
 
 	runner, err := osquery.NewRunner(*osqueryExecutable, *timeoutString)
 	if err != nil {
-		log.Fatalf("failed to create osquery runner:", err)
+		exitUnknown("failed to create osquery runner:", err)
 	}
 	if *query == "" {
 		log.Fatal("no osquery string provided as cmdline")
 	}
-	result, err := runner.Run(*query)
+	osqueryResult, err := runner.Run(*query)
 	if err != nil {
 		log.Fatal("failed to run osquery:", err)
 	}
-	if len(result.Items) != 1 {
-		log.Fatalf("expected one resultset from osquery, got %d", len(result.Items))
+	if len(osqueryResult.Items) != 1 {
+		exitUnknown("expected one resultset from osquery, got %d", len(osqueryResult.Items))
 	}
 
-	data := result.Items[0]
+	data := osqueryResult.Items[0]
 
-	if _, ok := data["count"]; !ok {
-		log.Fatalf("osquery result doesn't contain a key 'count'")
+	if _, ok := data[*resultKey]; !ok {
+		// TODO: adapt error message
+		exitUnknown("osquery osqueryResult doesn't contain a key 'result'")
 	}
-	count, err := strconv.ParseFloat(data["count"], 32)
+	result, err := strconv.ParseFloat(data[*resultKey], 32)
 	if err != nil {
-		log.Fatalf("count key/value pair can't be parsed as float")
+		exitUnknown("result key/value pair can't be parsed as float")
 	}
 	severity := gonag.OK
 	if *warn != "" {
 		if warning, err := strconv.ParseFloat(*warn, 32); err == nil {
-			if count > warning {
+			if result > warning {
 				severity = gonag.WARNING
 			}
 		}
 	}
 	if *crit != "" {
 		if critical, err := strconv.ParseFloat(*crit, 32); err == nil {
-			if count > critical {
+			if result > critical {
 				severity = gonag.CRITICAL
 			}
 		}
@@ -81,26 +81,26 @@ func main() {
 	resultText := bytes.NewBuffer([]byte(``))
 	template.Execute(resultText, &data)
 
-	_, countUOM, err := gonag.ParseValue(data["count"])
+	_, resultUOM, err := gonag.ParseValue(data[*resultKey])
 	if err != nil {
-		log.Fatalf("failed to parse count uom")
+		exitUnknown("failed to parse result uom")
 	}
-	runtime, timeUOM, err := gonag.ParseValue(fmt.Sprintf("%fs",result.Runtime.Seconds()))
+	runtime, timeUOM, err := gonag.ParseValue(fmt.Sprintf("%fs", osqueryResult.Runtime.Seconds()))
 	if err != nil {
-		log.Fatalf("failed to parse runtime uom from %s", fmt.Sprintf("%fs",result.Runtime.Seconds()))
+		exitUnknown("failed to parse runtime uom from %s", fmt.Sprintf("%fs", osqueryResult.Runtime.Seconds()))
 	}
 	nagiosResult := gonag.CheckResult{
 		ReturnCode: severity,
 		Text: resultText.String(),
 		Perfdata: gonag.Perfdata{
 			{
-				Label: "count",
-				Value: data["count"],
+				Label: "result",
+				Value: data[*resultKey],
 				Thresholds: gonag.Thresholds{
 					gonag.Warn: *warn,
 					gonag.Crit: *crit,
 				},
-				UOM: countUOM,
+				UOM: resultUOM,
 			},
 			{
 				Label: "runtime",
@@ -113,8 +113,20 @@ func main() {
 	}
 	outputString, err := nagiosResult.RenderCheckResult(gonag.NagiosMRPEFormatter)
 	if err != nil {
-		log.Fatal("failed to render check result as nagios check")
+		log.Fatal("failed to render check osqueryResult as nagios check")
 	}
 	fmt.Println(outputString)
 	os.Exit(int(nagiosResult.ReturnCode))
+}
+
+func exitUnknown(msgFormat string, a ...interface{}) {
+	log.Errorf(msgFormat, a...)
+	r := gonag.CheckResult{
+		ReturnCode: gonag.UNKNOWN,
+		Text: fmt.Sprintf(msgFormat, a...),
+		Perfdata: gonag.Perfdata{},
+	}
+	returnString, _ := r.RenderCheckResult(gonag.NagiosMRPEFormatter)
+	fmt.Println(returnString)
+	os.Exit(int(r.ReturnCode))
 }
